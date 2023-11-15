aac6da7e8fbc27bca656fa3e6f642c7042b58a46
jesus <jdrd123.12345@gmail.com>
2023-11-14 19:22:44 -0500

    se cambio el login y register(error)

---
+++ funciones.js
@@ -0,0 +1,12 @@
+const algo = document.getElementById("boton");
+algo.addEventListener("click", funcion);
+var aasas = [];
+function funcion() {
+  aasas = document.getElementById(nombre).value;
+  alert(aasas);
+  
+
+};
+function egsegeg(){
+  alert(aasas);
+};
