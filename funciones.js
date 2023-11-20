e0b5f561d52aaee282ad835a33bb7ff7f4db9b0f
jesus <jdrd123.12345@gmail.com>
2023-11-19 20:43:34 -0500

    se añadio javascript a register

--- funciones.js
+++ funciones.js
@@ -1,12 +1,18 @@
-const algo = document.getElementById("boton");
-algo.addEventListener("click", funcion);
-var aasas = [];
-function funcion() {
-  aasas = document.getElementById(nombre).value;
-  alert(aasas);
-  
+function asa() {
+  class persona {
+  constructor(nombre, apellido, gmail, contraseña){
+    this.nombre = document.getElementById('nombre').value
+    this.apellido = document.getElementById('apellido').value
+    this.gmail = document.getElementById('gmail').value
+    this.contraseña = document.getElementById('contraseña').value
+    this.genero = document.getElementById('genero').value
+  };
+  };
+  var personauno = new persona(nombre, apellido, gmail, contraseña, genero)
+  console.log(personauno)
+  localStorage.setItem('personauno', 'ssss');
+  location.href ='login.html';
+  return false;
+}
 
-};
-function egsegeg(){
-  alert(aasas);
-};
+
